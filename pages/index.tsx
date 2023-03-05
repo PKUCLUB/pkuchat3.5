f4e5d021cf3054f99305abde2475ffffb9efbc77
PKUCLUB <126936244+PKUCLUB@users.noreply.github.com>
2023-03-05 11:07:41 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,16 +16,17 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center">
         <Markdown
           disableCopy
-          children={`### 未名俱乐部 - My ChatGPT
+          children={`### 未名俱乐部 My ChatGPT
+
+未名俱乐部基于OpenAI官方ChatGPT API开发的My ChatGPT，能够帮助您在本土网络环境下与ChatGPT聊天机器人自由互动，并获得广泛信息。
 
 
-My ChatGPT是未名俱乐部基于OpenAI官方ChatGPT API而开发的聊天机器人，能够帮您在本土网络环境下与ChatGPT官方聊天机器人自由互动，并获得广泛信息。
+未名俱乐部是北大、清华校友发起，包含众多海内外知名高校校友的高端社群，以就业创业、投融资、联谊交友为交流主题！欢迎各高校朋友加入社群，我们的微信公众号：pkuclub，联系人微信：pkuboyata。
 
 
-未名俱乐部是由北大、清华校友发起，包含众多海内外知名高校校友的高端交流平台！我们的微信公众号ID：pkuclub，社群联系人：pkuboyata
+致谢：源代码作者北京大学[Gabriel Wu](https://github.com/lucifer1004)为本站提供技术支持。
 
-
-感谢[Gabriel Wu](https://github.com/lucifer1004)提供技术支持。`}
+提示：建议使用chrom、edge、firefox等浏览器打开本网站。`}
         />
         <Button
           onClick={() => {
