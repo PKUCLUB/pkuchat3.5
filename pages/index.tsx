3f5bf1c2a3062f0c1bc98622e35f28225213440d
PKUCLUB <126936244+PKUCLUB@users.noreply.github.com>
2023-03-05 12:55:42 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,11 +18,11 @@ export default function Home() {
           disableCopy
           children={`### 未名俱乐部 My ChatGPT
 
-未名俱乐部基于OpenAI官方ChatGPT API开发的My ChatGPT，能够帮助您在本土网络环境下与ChatGPT聊天机器人自由互动，并获得广泛信息。
+未名俱乐部基于OpenAI官方ChatGPT API开发的My ChatGPT，能够帮助您与ChatGPT聊天机器人自由互动，获得广泛信息。
 
-未名俱乐部是北大、清华校友发起，包含众多海内外知名高校校友的高端社群，以就业创业、投融资、联谊交友为交流主题！欢迎各高校朋友加入社群，我们的微信公众号：pkuclub，联系人微信：pkuboyata。
+未名俱乐部是北大清华校友发起，包含众多海内外知名高校校友的高端社群，以就业创业、投融资、联谊交友为主题！欢迎各高校朋友加入，公众号：pkuclub，联系人：pkuboyata（添加微信发送：学校-专业学历-姓名）。
 
-致谢：源代码作者北京大学[Gabriel Wu](https://github.com/lucifer1004)为本站提供技术支持。
+致谢：北京大学[Gabriel Wu](https://github.com/lucifer1004)博士为本站提供源代码及技术支持。
 
 提示：建议使用chrome、edge、firefox等浏览器打开本网站，效果最佳。`}
         />
